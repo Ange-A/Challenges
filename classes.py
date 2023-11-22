f011778ec05cdec9ba6fec9faabe19ec9e5c2056
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-11-22 14:38:11 +0200

    oop

--- classes.py
+++ classes.py
@@ -2,5 +2,24 @@ class MyClass(object):
     pass
 
 this_obj = MyClass() #instance
-
+######################
 print(this_obj)
+
+class Joe(object):
+    greeting = 'hello, Joe'
+    
+thisjoe = Joe()
+
+print(thisjoe.greeting)
+
+##############
+
+class Joe(object):
+    def callme(self):
+        print('calling "callme" method with instance: ')
+        
+thisjoe = Joe()
+
+thisjoe.callme()
+
+print; print
