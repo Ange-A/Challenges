f1ffc37748c5b5bd5d548af738f46ef25adbddc7
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-11-22 15:41:14 +0200

    oop

--- classes.py
+++ classes.py
@@ -23,3 +23,17 @@ thisjoe = Joe()
 thisjoe.callme()
 
 print; print
+
+###############A method on an instance passes instance as the argument to the method(named seld in the method)
+
+import random
+class MyClass(object):
+    def dothis(self):
+        self.rand_val = random.randint(1, 10)
+        
+myinst = MyClass()
+myinst.dothis()
+print(myinst.rand_val)
+
+
+        
