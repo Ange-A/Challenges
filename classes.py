c9c800ba9709bc8e6edc5af409f8a584b5592759
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-11-22 15:59:43 +0200

    oop

--- classes.py
+++ classes.py
@@ -36,4 +36,42 @@ myinst.dothis()
 print(myinst.rand_val)
 
 
+class MyClass(object):
+    def set_val(self, val):
+        self.value = val
         
+    def get_val(self):
+        return self.value   
+    
+a = MyClass()
+b = MyClass()   
+a.value = 'hello'
+
+a.set_val(10)
+b.set_val(100)
+
+
+print(a.get_val())
+print(b.get_val())
+
+############################
+
+class MyInteger(object):
+    def set_val(self, val):
+        try:
+            val = int(val)
+        except ValueError:
+            return
+        self.val = val
+        
+    def get_val(self):
+        return self.val
+    
+    def increment_val(self):
+        self.val= self.val + 1
+        
+i = MyInteger()
+i.set_val(9)
+print(i.get_val())
+i.set_val('hi')
+print(i.get_val())
