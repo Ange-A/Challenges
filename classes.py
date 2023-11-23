7a5e5bc904461c93243824fbc8d09897cae4fd94
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-11-23 15:44:49 +0200

    oop

--- classes.py
+++ classes.py
@@ -163,16 +163,25 @@ print(b.get_list())
 class Animal(object):
     def __init__(self, name):
         self.name = name
+    
     def eat(self, food):
-        print('%s is earing %s!'%(self.name, food))
+        print('%s is eating %s!'%(self.name, food))
 
 class Dog(Animal):
     def fetch(self, thing):
         print('%s goes after the %s!' %(self.name, thing))
         
-
 class Cat(Animal):
     def swatstring(self):
-        print('%s shreds the string!' %(self.name, thing))
+        print('%s shreds the string!' %(self.name))
+        
+    
+r = Dog('Rover')
+f = Cat('Fluffy')
+
+r.fetch('paper')
+f.swatstring()
+r.eat('dog food')
+r.eat('some food')
         
     
