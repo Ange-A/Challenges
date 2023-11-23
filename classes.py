b8ca3d86048c902f6cd56f642004f46aff4a7ae9
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-11-23 15:37:30 +0200

    oop

--- classes.py
+++ classes.py
@@ -159,5 +159,20 @@ b.push("go")
 print(a.get_list())
 print(b.get_list())
     
+######################
+class Animal(object):
+    def __init__(self, name):
+        self.name = name
+    def eat(self, food):
+        print('%s is earing %s!'%(self.name, food))
 
+class Dog(Animal):
+    def fetch(self, thing):
+        print('%s goes after the %s!' %(self.name, thing))
+        
+
+class Cat(Animal):
+    def swatstring(self):
+        print('%s shreds the string!' %(self.name, thing))
+        
     
