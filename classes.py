a3ab0efd0c1928c5450ca4981cd514d1c2584acc
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-11-27 19:49:27 +0200

    oop

--- classes.py
+++ classes.py
@@ -303,5 +303,18 @@ class GetSetInt(GetSetParent):
             super(GetSetInt, self).set_val(value)
     def showdoc(self):
         print('GetSeInt object({0}), only accepts integer values'.format(id(self)))
-     
+        
+        
+class GetSetList(GetSetParent):
+    def __init__(self, value= 0):
+        self.vallist = [value]
+    def get_val(self):
+        return self.vallist[-1]
+    def get_vals(self):
+        return self.vallist
+    def set_val(self, value):
+        self.vallist.append(value)
+    def showdoc(self):
+        print('getlist object, len{0}, stores, history of values set'.format(len(self.vallist)))
+    
         
