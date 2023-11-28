18e8c6deca92665ad3a8f5ed870e4cd2b4823fe6
Ange-A <33396595+Ange-A@users.noreply.github.com>
2023-11-28 16:18:03 +0200

    oop

--- classes.py
+++ classes.py
@@ -390,7 +390,27 @@ print(x)
 x.append('spam')
     
 print(x[1])
+
+############################
+
+class MyClass(object):
+    
+    def __enter__(self):
+        print('we have entered "with"')
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        print('we are leaving "with"')
         
+   
+    def sayhi(self):
+        print('hi, instance %s' %(id(self)))
+        
+cc = MyClass()
+with MyClass() as cc:
+    cc.sayhi()
+    
+print('after the "with" block')
 
 
         
